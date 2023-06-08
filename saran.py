5b5988de208f161ff5b42f91d7dd37a2751a4119
Saravanakumar R <saravanakumar.msc.noc@gmail.com>
2023-06-08 10:15:33 +0000

    login saran

---
+++ saran.py
@@ -0,0 +1,19 @@
+>>> freecodecamp = "freeCodeCamp"
+
+>>> freecodecamp[0:9:2]
+'feCdC'
+
+>>> freecodecamp[2:10:3]
+'eoC'
+
+>>> freecodecamp[1:12:4]
+'roa'
+
+>>> freecodecamp[4:8:2]
+'Cd'
+
+>>> freecodecamp[3:9:2]
+'eoe'
+
+>>> freecodecamp[1:10:5]
+'rd'
